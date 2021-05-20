6c7d27b13e562c189a5aeb933b5885c4fd364930
DCarr40 <83989305+DCarr40@users.noreply.github.com>
2021-05-20 18:59:46 -0400

    created davidAccount object in account.js

--- account.js
+++ account.js
@@ -1,4 +1,11 @@
 "use strict"
 
 //This should create an obj
-let account1 = new Account();
+let davidAccount = {
+
+    firstname: "David",
+    lastname: "Carruthers",
+    accountPin : 12345 ,
+    balance: 1000
+
+};
