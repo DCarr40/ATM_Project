eace6c4f41b44d943993988228b568c59aeb4f44
DCarr40 <83989305+DCarr40@users.noreply.github.com>
2021-05-20 20:44:40 -0400

    added yesNo, promptFor, and accessAcctYesNo funct to index.js

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
 "use strict"
-let async = require("async");
-const yesno = require('yesno'); 
-const { davidAccount } = require('./account');
+
 const { getBal, deposit, withdraw, valPin } = require('./atm');
 
-
 // this line works console.log(module);
-// this line works console.log(getBal(davidAccount));
+console.log(getBal(davidAccount));
 
 
 //User Menu Starts Below
-const accessAccountYesNo = await yesno({
-    question = prompt("Hellow would you like to access your account?")
-});
 
 
+function mainMenu(){
+    alert("Hello Welcome to the Bank ATM Menu");
+    accessAcctYesNo(input);
+
+    switch(input){
+        case "yes":
+            //TODO 
+        break;
+        case "no":
+            //TODO 
+        break;
+        default:
+        return mainMenu(); // ask again
+    }
+}
+
+function accessAcctYesNo(input){
+    input = promptFor("Would you like to access your account? Enter 'yes' or 'no'", yesNo).toLowerCase;
+}
+
+function yesNo(input){
+    return input.toLowerCase() == "yes" || input.toLowerCase() == "no";
+}
+
+function promptFor(question, valid){
+    do{
+      var response = prompt(question).trim();
+    } while(!response || !valid(response));
+    return response;
+}
 
 //amount = parseInt(prompt("How much money would you like to withdraw?"));
 
